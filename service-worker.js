218eea0b349c4615bb227ebac62b2f0a0b15c759
trailpodder <jukka.a.kukkonen@gmail.com>
2025-06-20 10:05:33 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
-// service worker placeholder
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open('endurosync-v1').then(cache => {
+    return cache.addAll([
+      './',
+      './index.html',
+      './main.js',
+      './manifest.json',
+      './icon.png'
+    ]);
+  }));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
