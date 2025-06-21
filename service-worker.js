954f994bcc796ac7696780cc85423ae0ea562a4c
trailpodder <jukka.a.kukkonen@gmail.com>
2025-06-21 18:10:38 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,29 @@
-const CACHE_NAME = 'endurosync-cache-v1';
-const ASSETS = [
-  './',
+const CACHE_NAME = 'endurosync-v1';
+const urlsToCache = [
+  '/',
   'index.html',
   'style.css',
   'main.js',
   'manifest.json',
-  'icon.png',
+  'togeojson.min.js',
   'nuts300.gpx',
+  'icon.png',
   'https://unpkg.com/leaflet/dist/leaflet.css',
   'https://unpkg.com/leaflet/dist/leaflet.js',
-  'https://unpkg.com/togeojson@0.16.0/dist/togeojson.min.js'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
