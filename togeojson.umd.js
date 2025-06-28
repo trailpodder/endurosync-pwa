b83595200448f00bb5390ca77a472ea6b2ce1b87
trailpodder <jukka.a.kukkonen@gmail.com>
2025-06-28 20:46:55 +0300

    Update togeojson.umd.js

--- togeojson.umd.js
+++ togeojson.umd.js
@@ -1,75 +1,102 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.toGeoJSON = {}));
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.togeojson = {}));
 })(this, (function (exports) {
   'use strict';
 
-  function getTracks(doc) {
-    const tracks = [];
-    const trks = doc.getElementsByTagName('trk');
-    for (let i = 0; i < trks.length; i++) {
-      const trksegs = trks[i].getElementsByTagName('trkseg');
-      const coords = [];
-      for (let j = 0; j < trksegs.length; j++) {
-        const trkpts = trksegs[j].getElementsByTagName('trkpt');
-        for (let k = 0; k < trkpts.length; k++) {
-          const pt = trkpts[k];
-          const lat = parseFloat(pt.getAttribute('lat'));
-          const lon = parseFloat(pt.getAttribute('lon'));
-          const ele = pt.getElementsByTagName('ele')[0];
-          const elevation = ele ? parseFloat(ele.textContent) : undefined;
-          coords.push([lon, lat, elevation]);
-        }
+  function get(node, tag) {
+    return node.getElementsByTagName(tag);
+  }
+
+  function attr(node, attrName) {
+    return node.getAttribute(attrName);
+  }
+
+  function nodeVal(node) {
+    if (!node) return '';
+    return node.textContent || node.innerText || '';
+  }
+
+  function get1(node, tag) {
+    return get(node, tag)[0];
+  }
+
+  function coordPair(node) {
+    return [parseFloat(attr(node, 'lon')), parseFloat(attr(node, 'lat'))];
+  }
+
+  function coordPairEle(node) {
+    const coords = coordPair(node);
+    const ele = get(node, 'ele');
+    if (ele.length) coords.push(parseFloat(nodeVal(ele[0])));
+    return coords;
+  }
+
+  function getLine(node, options) {
+    const line = [];
+    const pts = get(node, 'trkpt');
+    for (let i = 0; i < pts.length; i++) {
+      line.push(coordPairEle(pts[i]));
+    }
+    return line;
+  }
+
+  function extractTrk(trk, options) {
+    const nameEl = get1(trk, 'name');
+    const name = nameEl ? nodeVal(nameEl) : undefined;
+
+    const line = [];
+    const segments = get(trk, 'trkseg');
+    for (let i = 0; i < segments.length; i++) {
+      line.push(getLine(segments[i], options));
+    }
+
+    return {
+      type: 'Feature',
+      properties: { name: name },
+      geometry: {
+        type: 'MultiLineString',
+        coordinates: line
       }
-      tracks.push({
-        type: 'Feature',
-        geometry: {
-          type: 'LineString',
-          coordinates: coords
-        },
-        properties: {}
-      });
-    }
-    return tracks;
+    };
   }
 
-  function getWaypoints(doc) {
-    const wpts = doc.getElementsByTagName('wpt');
-    const features = [];
-    for (let i = 0; i < wpts.length; i++) {
-      const wpt = wpts[i];
-      const lat = parseFloat(wpt.getAttribute('lat'));
-      const lon = parseFloat(wpt.getAttribute('lon'));
-      const ele = wpt.getElementsByTagName('ele')[0];
-      const elevation = ele ? parseFloat(ele.textContent) : undefined;
-      const name = wpt.getElementsByTagName('name')[0];
-      const properties = {};
-      if (name) properties.name = name.textContent;
-      features.push({
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [lon, lat, elevation]
-        },
-        properties
-      });
-    }
-    return features;
+  function extractWpt(wpt) {
+    const props = {};
+    const nameEl = get1(wpt, 'name');
+    if (nameEl) props.name = nodeVal(nameEl);
+
+    return {
+      type: 'Feature',
+      properties: props,
+      geometry: {
+        type: 'Point',
+        coordinates: coordPairEle(wpt)
+      }
+    };
   }
 
-  function gpx(doc) {
+  function gpx(doc, options = {}) {
+    const tracks = get(doc, 'trk');
+    const waypoints = get(doc, 'wpt');
+
     const features = [];
-    features.push(...getTracks(doc));
-    features.push(...getWaypoints(doc));
+
+    for (let i = 0; i < tracks.length; i++) {
+      features.push(extractTrk(tracks[i], options));
+    }
+
+    for (let j = 0; j < waypoints.length; j++) {
+      features.push(extractWpt(waypoints[j]));
+    }
+
     return {
       type: 'FeatureCollection',
-      features
+      features: features
     };
   }
 
   exports.gpx = gpx;
-
-  Object.defineProperty(exports, '__esModule', { value: true });
 
 }));
